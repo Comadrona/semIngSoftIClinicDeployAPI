b30a50fcf5fb7aeb0ce304999204fc79d19e1956
Comadrona <joseluis120802.2@gmail.com>
2023-10-15 15:22:44 -0600

    Add allowedOptions Vercel Link

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,4 +1,5 @@
 const allowedOrigins = [
-    'https://seming-beautyclinic.onrender.com'
+    'https://seming-beautyclinic.onrender.com',
+    'https://sem-ing-soft-i-clinic-deploy.vercel.app/'
 ]
 module.exports = allowedOrigins;
